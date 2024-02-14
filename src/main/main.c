7818c5318483a488a44b7b1c662458926369ad91
Zarak Tariq <zaraktariq7@gmail.com>
2024-02-14 16:45:28 +0000

    rework of main.c

--- src/main/main.c
+++ src/main/main.c
@@ -1,23 +1,26 @@
 // main.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "token.h"
 
-int main (int argc, char **argv) {
-    // ensure user calls the program correctly 
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <input>\n", argv[0]);
         return EXIT_FAILURE;
     }
-    
-    char *input = argv[1];
 
-    // Process tokens from input
-    struct token_t current_token;
-    while ((current_token = get_next_token(&input)).type != END_OF_FILE) {
-        // Print token information
-        printf("Token Class: %s, Text: %s\n", token_type_to_string(current_token.type), current_token.text);
+    char *token = generateSpaceless(argv[1]);
+
+    while (token != NULL) {
+        TOKEN returnToken = parseToken(token);
+        printf("CLASS: %s, TEXT: %s\n", tokenTypeToString(returnToken.type), returnToken.text);
+        token = strtok(NULL, " ");
     }
 
-    return 0;
+    // Add an END_OF_FILE token
+    TOKEN eofToken = {END_OF_FILE, "EOF"};
+    printf("CLASS: %s, TEXT: %s\n", tokenTypeToString(eofToken.type), eofToken.text);
+
+    return EXIT_SUCCESS;
 }
