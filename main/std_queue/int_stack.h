c3c41383fb8a097167dccf99285cd76710cc9e02
Zarak Tariq <zaraktariq7@gmail.com>
2024-02-20 21:32:19 +0000

    better rework of stack.h

--- main/std_queue/int_stack.h
+++ main/std_queue/int_stack.h
@@ -23,17 +23,18 @@ extern int int_stack_pop(int_stack_t *stk, int *top_value);
 extern int int_stack_top(int_stack_t *stk, int *top_value);
 extern int int_stack_dup(int_stack_t *stk);
 extern int int_stack_swap(int_stack_t *stk);
-extern void int_stack_over(IntStack *s);
-extern void int_stack_rot(IntStack *s);
-extern void int_stack_drop(IntStack *s);
-extern void int_stack_2swap(IntStack *s);
-extern void int_stack_2dup(IntStack *s);
-extern void int_stack_2over(IntStack *s);
-extern void int_stack_2drop(IntStack *s);
 extern int int_stack_add(int_stack_t *stk);
 extern void int_stack_print(int_stack_t *stk, FILE *file);
 extern int int_stack_size(int_stack_t* stk);
 extern int int_stack_capacity(int_stack_t* stk);
 
-#endif // INT_STACK_H
+// New FORTH language stack operations
+extern int int_stack_over(int_stack_t *stk);
+extern int int_stack_rot(int_stack_t *stk);
+extern int int_stack_drop(int_stack_t *stk);
+extern int int_stack_2swap(int_stack_t *stk);
+extern int int_stack_2dup(int_stack_t *stk);
+extern int int_stack_2over(int_stack_t *stk);
+extern int int_stack_2drop(int_stack_t *stk);
 
+#endif // INT_STACK_H
